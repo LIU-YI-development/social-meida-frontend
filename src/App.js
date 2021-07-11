08b65198a4423dd7cfec02bddee5c3fa4b423860
Yi LIU <julien.ly@gmail.com>
2021-07-11 23:01:20 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
+
+function App() {
+  return (
+    <>
+      <Topbar />
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Topbar from "../../components/topbar/Topbar";
+
+function Hpme() {
+    return (
+      <Topbar/>
+    );
+  }
+  
+  export default Hpme;
+  

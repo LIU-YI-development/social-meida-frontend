8712a26f655c668d1f8fd1a91e11c8a60c3eff91
Yi LIU <julien.ly@gmail.com>
2021-07-12 12:40:37 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Home from './pages/home/Home';
 import Sidebar from './components/sidebar/Sidebar';
-import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <>
-      <Topbar />
-      <Sidebar />
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import Topbar from '../../components/topbar/Topbar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.css'
+
+function Hpme() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar/>
+        <Feed />
+        <Rightbar/>
+
+      </div>
+  
+    </>
+  );
+}
+
+export default Hpme;
